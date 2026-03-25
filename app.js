e1eb4e9e954aa3b4f99f456d6ca064608f9d6b95
jonNTV <joni@ntv.is>
2026-03-25 20:14:33 +0000

    Tími 9

--- app.js
+++ app.js
@@ -1,20 +1,70 @@
-console.log("Hello world")
 
-let running = true;
+// for(let i = 10; i >= 0; i--){
+//    console.log(i);
+//}
 
-function getEqual(number){
+let i = 0;
 
-    for (let i = 1; i <= number; i++ ) {
-        if (i % 2 === 0){
-            console.log(`even ${i}`);
-        } else {
-            console.log(`odd ${i}`);
-            
-        }
-    }
-
-    return false;
+while(i < 5){
+    console.log(i);
+    i++;
 }
 
-getEqual(22)
+let win = false;
+
+while( win === false){
+    console.log("keyrir tölvuleikinn");
+
+    win = true;
+    console.log("JIBBI ÞU VANNST")
+}
+
+function greeting(){
+    console.log("Halló úr falli");
+}
+
+for(let i = 1; i <= 10; i++){
+    greeting();
+}
+
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+function greet(firstName, lastName){
+    return firstName + " " + lastName
+}
+
+console.log(greet("Ingvarsson", "Jón"));
+
+function add(num1, num2){
+    let sum = num1 + num2;
+    console.log(sum);
+    return sum;
+}
+
+function decrement(num1, num2){
+    console.log(num1 - num2)
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    console.log(num1 * num2)
+    return num1 * num2;
+}
+
+function isEven(num){
+    if(num % 2 === 0){
+        return true;
+    } else {
+        return false
+    }
+}
+
+add(18383, 737373)
+decrement(5, 10)
+multiply(10,10);
+
+console.log(isEven(123471274892373488))
+
 
